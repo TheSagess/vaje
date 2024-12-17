ec8cd158db8f2dae23237fe84345cecbde7d2eff
GitHub Action <action@github.com>
2024-12-17 13:44:38 +0000

    Initialize template repository proper
    Set package name to com.github.thesagess.vaje
    Set project name to vaje
    Set modid to vaje

--- src/main/java/com/github/thesagess/vaje/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/thesagess/vaje/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.thesagess.vaje.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
